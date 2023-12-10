16ccd0e77e7396c290105755888f1bcc220c2678
HualiangZ <zhualiang88@gmail.com>
2023-12-10 14:25:10 +0000

    added height indicator

--- Assets/AltiMeterScript.cs
+++ Assets/AltiMeterScript.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using static UnityEditor.ShaderData;
 public class AltiMeterScript : MonoBehaviour
 {
     public RawImage attitudeIndicator;
+    public TMP_Text height;
     public Flight playerZ;
     public Transform player;
     // Start is called before the first frame update
@@ -19,5 +23,6 @@ public class AltiMeterScript : MonoBehaviour
     {
         attitudeIndicator.uvRect = new Rect(0, player.localEulerAngles.x / 360, 1, 1);
         attitudeIndicator.transform.rotation = Quaternion.Euler(0f, 0f, -playerZ.altitudeZ);
+        height.text = "" + (int)Math.Round(player.transform.position.y);
     }
 }
